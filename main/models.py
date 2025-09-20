6375c040019d20f3f424ba4097fc033d9fb60894
Farrel Arrayyan <farrel0810@gmail.com>
2025-09-20 14:41:56 +0700

    Progress Tugas 4: Authentikasi, Session, dan Cookies

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     CATEGORY_CHOICES = [
@@ -11,6 +12,7 @@ class Product(models.Model):
         ('other', 'Other')
     ]
     
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     brand = models.CharField(max_length=255)
